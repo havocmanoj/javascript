329d097b381293d14965a755cf2fdc3e4ee10080
Mano <manoj78713797@gmail.com>
2025-12-08 00:50:38 +0530

    html file added

---
+++ script.js
@@ -0,0 +1,13 @@
+ 
+ let choclate = "New Element 123";
+ let apple = "Apple Element";
+
+ function greet() {
+        let apple = "Orange Element";
+        console.log("Local "+apple);
+     }
+
+ console.log("Global Apple Element:", apple );
+ console.log("Hello World");
+
+    greet();  
