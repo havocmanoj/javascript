e35e81d0e375782028ad8dc67b76d83efdb51f50
Mano <manoj78713797@gmail.com>
2025-12-10 16:48:02 +0530

    object destructure in function

--- script.js
+++ script.js
@@ -14,10 +14,12 @@
 
     const person = {
         firstName: "Mano",
+        lastName: "Kumar",
         age: 23,
         greet() {
             console.log("Hello, " + this.name);
-        }   
+        },
+        favColor: "blue"
     }
     const personMethod ={
         yearOfBirth() {
@@ -50,5 +52,19 @@
     //console.log(person)
 
     // Object Destructuring
-    const {firstName, age} = person;
-    console.log(firstName, age);
+    const {firstName, age, favColor = 'red', father_age= 12, lastName : myLastName} = person;
+    console.log(firstName, age, father_age, favColor, myLastName);
+    // here favColor and father_age values set to default and lastName is renamed to myLastName;
+
+    // destructure and use rest operator
+    let num1, num2;
+
+    ({num1, num2, ...others} = {num1: 10, num2: 20, num3: 30, num4: 40})
+    console.log(num1, num2, others);
+
+    // Destructuring in function parameters
+    function DisplayUser({firstName, age,  favColor: favouriteColor}) {
+        console.log(`Hello this is ${firstName} and my age is ${age}, My favourite color is ${favouriteColor}`);
+        
+    }
+    DisplayUser(person);
